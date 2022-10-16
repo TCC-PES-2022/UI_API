a30554bb1cd3418d92992a55704c88ed3b01f27d
afonsoh1 <afonso.almeida@engenharia.ufjf.br>
2022-10-16 14:40:53 -0300

    Teste Autenticacao

--- Teste_QT/Teste_QT/tst_teste_ui_api.cpp
+++ Teste_QT/Teste_QT/tst_teste_ui_api.cpp
@@ -10,9 +10,11 @@ class Teste_UI_API : public QObject
 public:
     Teste_UI_API();
     ~Teste_UI_API();
+
     st_ui_aut aut_gui;
     st_ui_image img_gui;
     st_ui_conexao con_gui;
+
     st_ui_image img_controler;
     st_ui_aut aut_controler;
     st_ui_conexao con_controler;
@@ -20,15 +22,21 @@ private slots:
 
     void test_Inicializacao_GUI();
     void test_Inicializacao_CTL();
-    void test_envio_autenticacao();
-    void test_recebimento_autenticacao();
-    void test_resposta_autenticacao();
+    //Switch Test for authentication proccess
+
+    void test_envio_autenticacao_ok();
+    void test_envio_autenticacao_falha();
+    void test_cadastrar_usuario_ok();
+    void test_cadastrar_usuario_falha();
+    void test_remover_usuario_ok();
+    void test_remover_usuario_falha();
+
     void test_reiniciar_mq_GUI();
     void test_reiniciar_mq_CTL();
+    //Switch Test for image directory proccess
 
-    void test_envio_diretorio_imagem();
-    void test_recebimento_diretorio_imagem();
-    void test_resposta_diretorio_imagem();
+    void test_envio_diretorio_imagem_ok();
+    void test_envio_diretorio_imagem_falha();
 };
 
 Teste_UI_API::Teste_UI_API()
@@ -77,42 +85,147 @@ void Teste_UI_API::test_Inicializacao_CTL()
     QCOMPARE(con_controler.byte_controle,0);
     QCOMPARE(con_controler.byte_controle_anterior,0);
 }
-void Teste_UI_API::test_envio_autenticacao()
+
+void Teste_UI_API::test_envio_autenticacao_ok()
 {
-    static st_ui_aut aut_gui;
-    aut_gui.byte_controle = UI_EnviarLogin;
-    memcpy(aut_gui.autenticao.login,"Afonso",sizeof("Afonso"));
-    memcpy(aut_gui.autenticao.senha,"123",sizeof("123"));
+    aut_gui.byte_controle = UI_EnviarLogin;     //Set Comamand
+    memcpy(aut_gui.autenticao.login,"Afonso",sizeof("Afonso"));     //Set Login
+    memcpy(aut_gui.autenticao.senha,"123",sizeof("123"));   //Set Password
+    aut_gui = *verificar_autenticacao_GUI(aut_gui.byte_controle, &aut_gui); //Send to ctr queue
+    QCOMPARE(aut_gui.byte_controle,UI_Aguardar);    //Comparing control byte
+    uint8_t val = verificarFilas(&img_controler, &aut_controler,&con_controler, UI__CTL); //Verifica fila do ctr
+    QCOMPARE(val,RP_fila_autenticacao); // Verifica o retorno da fila
+    QCOMPARE(aut_controler.byte_controle,UI_EnviarLogin); // compara o comando enviado
+    aut_controler = *verificar_autenticacao_Controler(UI_Ok,&aut_controler); // envia resposta para a GUI
+    QCOMPARE(aut_controler.byte_controle,UI_Aguardar);  // Verifica o estado da maquina de estados
+    QCOMPARE(memcmp(&aut_controler.autenticao.login,"Afonso",sizeof("Afonso")),0); // verifica o login enviado
+    QCOMPARE(memcmp(&aut_controler.autenticao.senha,"123",sizeof("123")),0);// verifica a senha enviada
+    val = verificarFilas(&img_gui, &aut_gui,&con_gui, UI__GUI);  // verifica a fila da gui
+    QCOMPARE(val,RP_fila_autenticacao);// Verifica retorno da fila
+    QCOMPARE(memcmp(&aut_gui.autenticao.login,"Afonso",sizeof("Afonso")),0); // verifica se houve alteração no login enviado
+    QCOMPARE(memcmp(&aut_gui.autenticao.senha,"123",sizeof("123")),0);// verifica se houve alteração na senha enviada
+    QCOMPARE(aut_gui.byte_controle_anterior,UI_EnviarLogin); // compara o comando solicitado
+    QCOMPARE(aut_gui.byte_controle,UI_Ok); // compara o comando enviado
+}
+
+void Teste_UI_API::test_envio_autenticacao_falha()
+{
+    aut_gui.byte_controle = UI_EnviarLogin;     //Set Comamand
+    memcpy(aut_gui.autenticao.login,"Afonso",sizeof("Afonso"));     //Set Login
+    memcpy(aut_gui.autenticao.senha,"123",sizeof("123"));   //Set Password
+    aut_gui = *verificar_autenticacao_GUI(aut_gui.byte_controle, &aut_gui); //Send to ctr queue
+    QCOMPARE(aut_gui.byte_controle,UI_Aguardar);    //Comparing control byte
+    uint8_t val = verificarFilas(&img_controler, &aut_controler,&con_controler, UI__CTL); //Verifica fila do ctr
+    QCOMPARE(val,RP_fila_autenticacao); // Verifica o retorno da fila
+    QCOMPARE(aut_controler.byte_controle,UI_EnviarLogin); // compara o comando enviado
+    aut_controler = *verificar_autenticacao_Controler(UI_Falha,&aut_controler); // envia resposta para a GUI
+    QCOMPARE(aut_controler.byte_controle,UI_Aguardar);  // Verifica o estado da maquina de estados
+    QCOMPARE(memcmp(&aut_controler.autenticao.login,"Afonso",sizeof("Afonso")),0); // verifica o login enviado
+    QCOMPARE(memcmp(&aut_controler.autenticao.senha,"123",sizeof("123")),0);// verifica a senha enviada
+    val = verificarFilas(&img_gui, &aut_gui,&con_gui, UI__GUI);  // verifica a fila da gui
+    QCOMPARE(val,RP_fila_autenticacao);// Verifica retorno da fila
+    QCOMPARE(memcmp(&aut_gui.autenticao.login,"Afonso",sizeof("Afonso")),0); // verifica se houve alteração no login enviado
+    QCOMPARE(memcmp(&aut_gui.autenticao.senha,"123",sizeof("123")),0);// verifica se houve alteração na senha enviada
+    QCOMPARE(aut_gui.byte_controle_anterior,UI_EnviarLogin); // compara o comando solicitado
+    QCOMPARE(aut_gui.byte_controle,UI_Falha); // compara o comando enviado
+}
+void Teste_UI_API::test_cadastrar_usuario_ok()
+{
+     aut_gui.byte_controle = UI_Cadastrar;
+     memcpy(aut_gui.autenticao.login,"Afonso",sizeof("Afonso"));     //Set Login
+     memcpy(aut_gui.autenticao.senha,"123",sizeof("123"));   //Set Password
+
+     aut_gui = *verificar_autenticacao_GUI(aut_gui.byte_controle, &aut_gui);
+     QCOMPARE(aut_gui.byte_controle,UI_Aguardar);
+
+     uint8_t val = verificarFilas(&img_controler, &aut_controler,&con_controler, UI__CTL); //Verifica fila do ctr
+     QCOMPARE(val,RP_fila_autenticacao); // Verifica o retorno da fila
+     QCOMPARE(aut_controler.byte_controle,UI_Cadastrar); // compara o comando enviado
+     aut_controler = *verificar_autenticacao_Controler(UI_Ok,&aut_controler); // envia resposta para a GUI
+     QCOMPARE(aut_controler.byte_controle,UI_Aguardar);  // Verifica o estado da maquina de estados
+     QCOMPARE(memcmp(&aut_controler.autenticao.login,"Afonso",sizeof("Afonso")),0); // verifica o login enviado
+     QCOMPARE(memcmp(&aut_controler.autenticao.senha,"123",sizeof("123")),0);// verifica a senha enviada
+     val = verificarFilas(&img_gui, &aut_gui,&con_gui, UI__GUI);  // verifica a fila da gui
+     QCOMPARE(val,RP_fila_autenticacao);// Verifica retorno da fila
+     QCOMPARE(memcmp(&aut_gui.autenticao.login,"Afonso",sizeof("Afonso")),0); // verifica se houve alteração no login enviado
+     QCOMPARE(memcmp(&aut_gui.autenticao.senha,"123",sizeof("123")),0);// verifica se houve alteração na senha enviada
+     QCOMPARE(aut_gui.byte_controle_anterior,UI_Cadastrar); // compara o comando solicitado
+     QCOMPARE(aut_gui.byte_controle,UI_Ok); // compara o comando enviado
+}
+
+void Teste_UI_API::test_cadastrar_usuario_falha()
+{
+     aut_gui.byte_controle = UI_Cadastrar;
+     memcpy(aut_gui.autenticao.login,"Afonso",sizeof("Afonso"));     //Set Login
+     memcpy(aut_gui.autenticao.senha,"123",sizeof("123"));   //Set Password
+
+     aut_gui = *verificar_autenticacao_GUI(aut_gui.byte_controle, &aut_gui);
+     QCOMPARE(aut_gui.byte_controle,UI_Aguardar);
+     uint8_t val = verificarFilas(&img_controler, &aut_controler,&con_controler, UI__CTL); //Verifica fila do ctr
+
+     QCOMPARE(val,RP_fila_autenticacao); // Verifica o retorno da fila
+     QCOMPARE(aut_controler.byte_controle,UI_Cadastrar); // compara o comando enviado
+     aut_controler = *verificar_autenticacao_Controler(UI_Falha,&aut_controler); // envia resposta para a GUI
+     QCOMPARE(aut_controler.byte_controle,UI_Aguardar);  // Verifica o estado da maquina de estados
+     QCOMPARE(memcmp(&aut_controler.autenticao.login,"Afonso",sizeof("Afonso")),0); // verifica o login enviado
+     QCOMPARE(memcmp(&aut_controler.autenticao.senha,"123",sizeof("123")),0);// verifica a senha enviada
+     val = verificarFilas(&img_gui, &aut_gui,&con_gui, UI__GUI);  // verifica a fila da gui
+     QCOMPARE(val,RP_fila_autenticacao);// Verifica retorno da fila
+     QCOMPARE(memcmp(&aut_gui.autenticao.login,"Afonso",sizeof("Afonso")),0); // verifica se houve alteração no login enviado
+     QCOMPARE(memcmp(&aut_gui.autenticao.senha,"123",sizeof("123")),0);// verifica se houve alteração na senha enviada
+     QCOMPARE(aut_gui.byte_controle_anterior,UI_Cadastrar); // compara o comando solicitado
+     QCOMPARE(aut_gui.byte_controle,UI_Falha); // compara o comando enviado
+}
+
+void Teste_UI_API::test_remover_usuario_ok()
+{
+    aut_gui.byte_controle = UI_RemoverUsuario;
+    memcpy(aut_gui.autenticao.login,"Afonso",sizeof("Afonso"));     //Set Login
+    memcpy(aut_gui.autenticao.senha,"123",sizeof("123"));   //Set Password
+
     aut_gui = *verificar_autenticacao_GUI(aut_gui.byte_controle, &aut_gui);
     QCOMPARE(aut_gui.byte_controle,UI_Aguardar);
+    uint8_t val = verificarFilas(&img_controler, &aut_controler,&con_controler, UI__CTL); //Verifica fila do ctr
+
+    QCOMPARE(val,RP_fila_autenticacao); // Verifica o retorno da fila
+    QCOMPARE(aut_controler.byte_controle,UI_RemoverUsuario); // compara o comando enviado
+    aut_controler = *verificar_autenticacao_Controler(UI_Ok,&aut_controler); // envia resposta para a GUI
+    QCOMPARE(aut_controler.byte_controle,UI_Aguardar);  // Verifica o estado da maquina de estados
+    QCOMPARE(memcmp(&aut_controler.autenticao.login,"Afonso",sizeof("Afonso")),0); // verifica o login enviado
+    QCOMPARE(memcmp(&aut_controler.autenticao.senha,"123",sizeof("123")),0);// verifica a senha enviada
+    val = verificarFilas(&img_gui, &aut_gui,&con_gui, UI__GUI);  // verifica a fila da gui
+    QCOMPARE(val,RP_fila_autenticacao);// Verifica retorno da fila
+    QCOMPARE(memcmp(&aut_gui.autenticao.login,"Afonso",sizeof("Afonso")),0); // verifica se houve alteração no login enviado
+    QCOMPARE(memcmp(&aut_gui.autenticao.senha,"123",sizeof("123")),0);// verifica se houve alteração na senha enviada
+    QCOMPARE(aut_gui.byte_controle_anterior,UI_RemoverUsuario); // compara o comando solicitado
+    QCOMPARE(aut_gui.byte_controle,UI_Ok); // compara o comando enviado
 }
-void Teste_UI_API::test_recebimento_autenticacao()
+void Teste_UI_API::test_remover_usuario_falha()
 {
+    aut_gui.byte_controle = UI_RemoverUsuario;
+    memcpy(aut_gui.autenticao.login,"Afonso",sizeof("Afonso"));     //Set Login
+    memcpy(aut_gui.autenticao.senha,"123",sizeof("123"));   //Set Password
 
-    uint8_t val = verificarFilas(&img_controler, &aut_controler,&con_controler, UI__CTL);
-    cout << "Senha: " << aut_controler.autenticao.senha <<endl;
-    cout << "Login: " << aut_controler.autenticao.login <<endl;
+    aut_gui = *verificar_autenticacao_GUI(aut_gui.byte_controle, &aut_gui);
+    QCOMPARE(aut_gui.byte_controle,UI_Aguardar);
+    uint8_t val = verificarFilas(&img_controler, &aut_controler,&con_controler, UI__CTL); //Verifica fila do ctr
 
-    QCOMPARE(val,RP_fila_autenticacao);
-    QCOMPARE(aut_controler.byte_controle,UI_EnviarLogin);
-    aut_controler = *verificar_autenticacao_Controler(UI_Ok,&aut_controler);
-    QCOMPARE(aut_controler.byte_controle,UI_Aguardar);
-}
-
-void Teste_UI_API::test_resposta_autenticacao()
-{
-    uint8_t val = verificarFilas(&img_gui, &aut_gui,&con_gui, UI__GUI);
-    cout << "Senha: " << aut_gui.autenticao.senha <<endl;
-    cout << "Login: " << aut_gui.autenticao.login <<endl;
-    QCOMPARE(val,RP_fila_autenticacao);
-    QCOMPARE(aut_gui.byte_controle_anterior,UI_EnviarLogin);
-    QCOMPARE(aut_gui.byte_controle,UI_Ok);
-
+    QCOMPARE(val,RP_fila_autenticacao); // Verifica o retorno da fila
+    QCOMPARE(aut_controler.byte_controle,UI_RemoverUsuario); // compara o comando enviado
+    aut_controler = *verificar_autenticacao_Controler(UI_Falha,&aut_controler); // envia resposta para a GUI
+    QCOMPARE(aut_controler.byte_controle,UI_Aguardar);  // Verifica o estado da maquina de estados
+    QCOMPARE(memcmp(&aut_controler.autenticao.login,"Afonso",sizeof("Afonso")),0); // verifica o login enviado
+    QCOMPARE(memcmp(&aut_controler.autenticao.senha,"123",sizeof("123")),0);// verifica a senha enviada
+    val = verificarFilas(&img_gui, &aut_gui,&con_gui, UI__GUI);  // verifica a fila da gui
+    QCOMPARE(val,RP_fila_autenticacao);// Verifica retorno da fila
+    QCOMPARE(memcmp(&aut_gui.autenticao.login,"Afonso",sizeof("Afonso")),0); // verifica se houve alteração no login enviado
+    QCOMPARE(memcmp(&aut_gui.autenticao.senha,"123",sizeof("123")),0);// verifica se houve alteração na senha enviada
+    QCOMPARE(aut_gui.byte_controle_anterior,UI_RemoverUsuario); // compara o comando solicitado
+    QCOMPARE(aut_gui.byte_controle,UI_Falha); // compara o comando enviado
 }
 
 void Teste_UI_API::test_reiniciar_mq_GUI()
 {
-    std::cout << "+[CTL] Resetando MQ... " << endl;
     img_gui = *carregar_imagem_GUI(UI_Null, &img_gui);
     aut_gui = *verificar_autenticacao_GUI(UI_Null, &aut_gui);
     QCOMPARE(aut_gui.byte_controle_anterior,UI_Null);
@@ -123,7 +236,6 @@ void Teste_UI_API::test_reiniciar_mq_GUI()
 
 void Teste_UI_API::test_reiniciar_mq_CTL()
 {
-    std::cout << "+[CTL] Resetando MQ... " << endl;
     img_controler = *carregar_imagem_Controler(UI_Null, &img_controler);
     aut_controler = *verificar_autenticacao_Controler(UI_Null, &aut_controler);
     QCOMPARE(aut_controler.byte_controle_anterior,UI_Null);
@@ -132,23 +244,49 @@ void Teste_UI_API::test_reiniciar_mq_CTL()
     QCOMPARE(img_controler.byte_controle,UI_Null);
 }
 
-void Teste_UI_API::test_envio_diretorio_imagem()
+void Teste_UI_API::test_envio_diretorio_imagem_ok()
+{
+    img_gui.dir_img.diretorio_img = "/csr/afsfd/asdasd/afonso/qqq/892139291.bin"; // Diretório teste
+    img_gui.dir_img.tam_dir = sizeof("/csr/afsfd/asdasd/afonso/qqq/892139291.bin"); // tamanho do diretório
+    img_gui = *carregar_imagem_GUI(UI_Carregar_Imagem,&img_gui); // comando enviado
+    QCOMPARE(img_gui.byte_controle,UI_Aguardar); // status da maquina
+    //
+    uint8_t val = verificarFilas(&img_controler, &aut_controler,&con_controler, UI__CTL); // verificar a fila
+    QCOMPARE(val,RP_fila_imagem); // resposta da funcao acima
+    QCOMPARE(img_controler.byte_controle,UI_Carregar_Imagem); // comando recebido
+    img_controler = *carregar_imagem_Controler(UI_Ok,&img_controler); // comando de resposta enviado
+    QCOMPARE(img_controler.byte_controle,UI_Aguardar); // status da maquina
+    QCOMPARE(img_controler.dir_img.diretorio_img,"/csr/afsfd/asdasd/afonso/qqq/892139291.bin"); // diretório recebido
+    QCOMPARE(img_controler.dir_img.tam_dir,sizeof("/csr/afsfd/asdasd/afonso/qqq/892139291.bin"));//tamanhao recebido
+    // Reset
+    img_controler = *carregar_imagem_Controler(UI_Null, &img_controler); // reset da maquina
+    QCOMPARE(img_controler.byte_controle_anterior,UI_Null);// status da maquina
+    QCOMPARE(img_controler.byte_controle,UI_Null);// status da maquina
+    //
+    val = verificarFilas(&img_gui, &aut_gui,&con_gui, UI__GUI); // verifica a fila
+    QCOMPARE(val,RP_fila_imagem); // resposta da funcao acima
+    QCOMPARE(img_gui.byte_controle_anterior,UI_Carregar_Imagem); // comando anterior
+    QCOMPARE(img_gui.byte_controle,UI_Ok); // resposta recebida
+    // Reset
+    img_gui = *carregar_imagem_GUI(UI_Null, &img_gui); // reset da maquina
+    QCOMPARE(img_gui.byte_controle_anterior,UI_Null); // status da maquina
+    QCOMPARE(img_gui.byte_controle,UI_Null);// status da maquina
+
+}
+
+void Teste_UI_API::test_envio_diretorio_imagem_falha()
 {
     img_gui.dir_img.diretorio_img = "/csr/afsfd/asdasd/afonso/qqq/892139291.bin";
     img_gui.dir_img.tam_dir = sizeof("/csr/afsfd/asdasd/afonso/qqq/892139291.bin");
-
     img_gui = *carregar_imagem_GUI(UI_Carregar_Imagem,&img_gui);
     QCOMPARE(img_gui.byte_controle,UI_Aguardar);
-
-}
-void Teste_UI_API::test_recebimento_diretorio_imagem()
-{
+    //
     uint8_t val = verificarFilas(&img_controler, &aut_controler,&con_controler, UI__CTL);
     cout << "Diretorio: " << img_controler.dir_img.diretorio_img <<endl;
     cout << "Tam: " << img_controler.dir_img.tam_dir <<endl;
     QCOMPARE(val,RP_fila_imagem);
     QCOMPARE(img_controler.byte_controle,UI_Carregar_Imagem);
-    img_controler = *carregar_imagem_Controler(UI_Ok,&img_controler);
+    img_controler = *carregar_imagem_Controler(UI_Falha,&img_controler);
     QCOMPARE(img_controler.byte_controle,UI_Aguardar);
     QCOMPARE(img_controler.dir_img.diretorio_img,"/csr/afsfd/asdasd/afonso/qqq/892139291.bin");
     QCOMPARE(img_controler.dir_img.tam_dir,sizeof("/csr/afsfd/asdasd/afonso/qqq/892139291.bin"));
@@ -156,22 +294,19 @@ void Teste_UI_API::test_recebimento_diretorio_imagem()
     img_controler = *carregar_imagem_Controler(UI_Null, &img_controler);
     QCOMPARE(img_controler.byte_controle_anterior,UI_Null);
     QCOMPARE(img_controler.byte_controle,UI_Null);
-}
-
-void Teste_UI_API::test_resposta_diretorio_imagem()
-{
-    uint8_t val = verificarFilas(&img_gui, &aut_gui,&con_gui, UI__GUI);
+    //
+    val = verificarFilas(&img_gui, &aut_gui,&con_gui, UI__GUI);
     cout << "Diretorio: " << img_gui.dir_img.diretorio_img <<endl;
     cout << "Tam: " << img_gui.dir_img.tam_dir <<endl;
     QCOMPARE(val,RP_fila_imagem);
     QCOMPARE(img_gui.byte_controle_anterior,UI_Carregar_Imagem);
-    QCOMPARE(img_gui.byte_controle,UI_Ok);
-
+    QCOMPARE(img_gui.byte_controle,UI_Falha);
     // Reset
     img_gui = *carregar_imagem_GUI(UI_Null, &img_gui);
     QCOMPARE(img_gui.byte_controle_anterior,UI_Null);
     QCOMPARE(img_gui.byte_controle,UI_Null);
 }
+
 QTEST_APPLESS_MAIN(Teste_UI_API)
 
 #include "tst_teste_ui_api.moc"
